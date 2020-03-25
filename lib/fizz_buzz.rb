4c3b7b3bec2ab4a36dc8981147fd556d01da5271
pauline_barnades <barnades.pauline@gmail.com>
2020-03-25 10:39:47 +0100

    Fizzbuzz test fail to pass %5 and %15

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,13 +1,17 @@
 def fizz_buzz(number)
+  if number % 15 == 0
+    'fizz buzz'
+  else
+    number
+  end 
+  if number % 5 == 0
+    'buzz'
+  else
+    number
+  end
   if number % 3 == 0
     'fizz'
   else
     number
   end 
-
-if number % 5 == 0
-  'buzz'
-else
-  number
-end
 end
